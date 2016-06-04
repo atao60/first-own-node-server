fbe0de0f6de492af87d57a5dbbe2eb111f627f34
atao60 <p.m.raoul@gmail.com>
2016-06-04 04:01:01 +0200

    Add tests with chai

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 import http from 'http';
 import assert from 'assert';
+import chai from 'chai'; 
+const should = chai.should();
 
 import '../lib/index.js';
 
@@ -7,6 +9,15 @@ describe('Example Node Server', () => {
   it('should return 200', done => {
     http.get('http://127.0.0.1:1337', res => {
       assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+});
+
+describe('Example Node Server test with chai', () => {
+  it('returns 200', done => {
+    http.get('http://127.0.0.1:1337', res => {
+      res.statusCode.should.equal(200);
       done();
     });
   });
